3af7e17a6f3adce4cc950d92aef01ef64d3626e4
Anmol Mishra <68518640+AnmolMishra29@users.noreply.github.com>
2024-04-15 09:26:47 +0530

    Add files via upload

---
+++ Day 7/question1.js
@@ -0,0 +1,22 @@
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 50) + 1;
+};
+
+const generateRandomNumberPromise = () => {
+  return new Promise((resolve, reject) => {
+    const randomNumber = generateRandomNumber();
+    if (randomNumber % 5 === 0) {
+      resolve(randomNumber);
+    } else {
+      reject(new Error("The generated number is not a multiple of 5"));
+    }
+  });
+};
+
+generateRandomNumberPromise()
+  .then((randomNumber) => {
+    console.log("Generated random number:", randomNumber);
+  })
+  .catch((error) => {
+    console.error("Error:", error.message);
+  });
